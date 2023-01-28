2d42c455a8c479f1708f398f5f541fb0e14aa4f0
MercutioG <110576904+MercutioG@users.noreply.github.com>
2023-01-27 18:23:21 -0700

    FInished project
    JS HTML and CSS all finished

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-johnsonButton.addEventListener("mouseover", hideButton);
+document.body.onload = function () {pageLoad()}; //On load effect
+johnsonButton.addEventListener("mouseover", hideButton); //Removes the other candidate button
+textRemove.addEventListener("mousemove", hideButton); //Removes the other candidate button
+window.addEventListener("contextmenu", (e) => {e.preventDefault()}); //Disables context menu
+presidentImg.addEventListener("click", hideButton); //Plays a sound on click and removes the image
+nixonButton.addEventListener("click", voteNixon)
 
-function hideButton(){
+function pageLoad(){ //Activates on load
+    document.getElementById("textRemove").style.top = "-100px"
+}
+function voteNixon(){ //Creates movable button
     this.style.display = "none";
+    let temp = document.createElement("p")
+    temp.setAttribute("id", "postButton")
+    mainArea.appendChild(temp)
+}
+function hideButton(){ //Hides whatever is clicked or hovered
+    this.style.display = "none";
+}
+
+mainArea.onclick = function (event) {
+    let screenCoords = this.getBoundingClientRect();
+
+    let buttonCoords = {
+        top: event.clientY - screenCoords.top - mainArea.clientTop - postButton.clientHeight / 2 - 10,
+        left: event.clientX - screenCoords.left - mainArea.clientLeft - postButton.clientWidth / 2 - 10,
+    }
+
+    postButton.style.left = (300 + Number(buttonCoords.left)) + "px"
+    postButton.style.top = (100 + Number(buttonCoords.top)) + "px"
 }
